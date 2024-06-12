171258ff5e2bb259b2e73923ca13d7e90d5bf080
gvahe <gvahe@LAPTOP-JDN88TTV.mynetworksettings.com>
2024-06-12 01:12:53 -0400

    Figures Changed

--- src/Resources/Figures.java
+++ src/Resources/Figures.java
@@ -11,7 +11,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class Figures extends All_Functions {
-
+//Vahe
 	// Before Class
 	@BeforeClass
 	public void startBrowser() throws Exception {
