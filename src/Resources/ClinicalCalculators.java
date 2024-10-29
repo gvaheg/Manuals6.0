80890d850848282ed08cd7976a6efba8d9ba5854
vahep <vahep@VahePC.mynetworksettings.com>
2024-10-29 13:28:46 -0400

    Calculators Update 10/29/24

--- src/Resources/ClinicalCalculators.java
+++ src/Resources/ClinicalCalculators.java
@@ -155,11 +155,8 @@ public class ClinicalCalculators extends All_Functions  {
 			}
 			// End Close Cookies
 			Thread.sleep(2000);
-			try {
-				wd.findElement(By.xpath("//*[@id=\"access-confirmation-popup\"]/div/div/div/div/div[3]/a[1]")).click();
-			}catch(Exception e) {
-				System.out.println("Can't Close Prompt");
-				}
+			try {wd.findElement(By.xpath("//button[@class='ChineseModalPopup_languageSelectorPopupVersionButton__j7M_0']")).click();
+			}catch(Exception e) {System.out.println("Can't Close Prompt");}
 			System.out.println("VERSION: PROD CN PV");
 			verifyClinicalCalculators();
 			wd.close();
